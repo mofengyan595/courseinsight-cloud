9bcc95d665efac6fe7811fa717f36dc2cc73f20f
mofengyan <3478843563@qq.com>
2026-07-31 16:16:04 +0800

    feat: initialize Spring Boot server with health endpoint

---
+++ courseinsight-server/src/test/java/com/courseinsight/server/CourseinsightServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.courseinsight.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseinsightServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
